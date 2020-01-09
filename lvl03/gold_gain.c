7bdec2698b6151f5ecc0d4f98d702ff9e5c4f00e
Olga Okrut <46659064+olgOk@users.noreply.github.com>
2020-01-09 10:34:27 -0800

    Update gold_gain.c
    Added solution to the problem

--- lvl03/gold_gain.c
+++ lvl03/gold_gain.c
@@ -87,3 +87,113 @@
 // (2,0) -> (1,1) -> (1,2) -> (0,3)
 // or
 // (2,0) -> (3,1) -> (2,2) -> (2,3)
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	gold_gain(int **mine, int n)
+{
+    int i = n - 2;
+    int j = 0;
+    int max_curr;
+    int score = 0;
+    if (n <= 0)
+        return (0);
+    if (n == 1)
+        return (mine[0][0]);
+    while(i >= 0)
+    {
+        j = 0;
+        while(j < n)
+        {
+            max_curr = 0;
+            if(j != 0 && mine[j - 1][i + 1] > max_curr)
+                max_curr = mine[j - 1][i + 1];
+            if (mine[j][i + 1] > max_curr)
+                max_curr = mine[j][i + 1];
+            if (j != n - 1 && mine[j + 1][i + 1] > max_curr)
+                max_curr = mine[j + 1][i + 1];
+            mine[j][i] += max_curr;
+            if( i == 0 && mine[j][i] > score)
+                score = mine[j][i];
+            ++j;
+        }
+        --i;
+    }
+    return (score);
+}
+
+int **convert_mine(int n, int mine[4][4])
+{
+    int **true_mine;
+    int i;
+    int j;
+
+    true_mine = (int **)malloc(sizeof(int *) * n);
+    i = 0;
+    while (i < n)
+    {
+        true_mine[i] = (int *)malloc(sizeof(int) * n);
+        j = 0;
+        while (j < n)
+        {
+            true_mine[i][j] = mine[i][j];
+            ++j;
+        }
+        ++i;
+    }
+    return (true_mine);
+}
+
+void    print_mine(int n, int **mine)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < n)
+    {
+        j = 0;
+        while (j < n)
+        {
+            printf("%3d", mine[i][j]);
+            ++j;
+        }
+        printf("\n");
+        ++i;
+    }
+}
+
+int main()
+{
+    int n = 4;
+    int mine[4][4] = {
+        { 1, 3, 1, 5 },
+        { 2, 2, 4, 1 },
+        { 5, 0, 2, 3 },
+        { 0, 6, 1, 2 }
+        };
+
+    // int n = 3;
+    // int mine[3][3] = {
+    //     { 1, 2, 3 },
+    //     { 3, 4, 8 },
+    //     { 9, 6, 7 }
+    //     };
+
+    // int n = 3;
+    // int mine[3][3] = {
+    //     { 1, 0, 0 },
+    //     { 0, 3, 4 },
+    //     { 0, 0, 0 }
+    //     };
+
+    int **true_mine = convert_mine(n, mine);
+
+    print_mine(n, true_mine);
+    printf("\n");
+    int answer = gold_gain(true_mine, n);
+    print_mine(n, true_mine);
+
+    printf ("%d\n", answer);
+}
